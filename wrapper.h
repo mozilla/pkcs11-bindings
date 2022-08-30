c8ebcf01828771da34e864eee23472544803c125
Dana Keeler <dkeeler@mozilla.com>
2022-08-30 13:53:59 -0700

    implement PKCS#11 Rust bindings

---
+++ wrapper.h
@@ -0,0 +1,21 @@
+/* -*- Mode: C++; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
+/* vim: set ts=2 et sw=2 tw=80: */
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#if defined(_WIN32) || defined(_WINDOWS)
+#  pragma pack(push, cryptoki, 1)
+#endif
+
+#define CK_PTR *
+#define CK_DECLARE_FUNCTION(returnType, name) returnType name
+#define CK_DECLARE_FUNCTION_POINTER(returnType, name) returnType(*name)
+#define CK_CALLBACK_FUNCTION(returnType, name) returnType(*name)
+#define NULL_PTR 0
+
+#include "pkcs11.h"
+
+#if defined(_WIN32) || defined(_WINDOWS)
+#  pragma pack(pop, cryptoki)
+#endif
